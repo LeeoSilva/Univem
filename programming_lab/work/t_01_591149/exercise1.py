f53da7acd38c648c124b1722cb85f86e5d5dd521
LeeoSilva <leeosilva0909@gmail.com>
2019-03-13 03:40:43 +0000

    t_01_591149 exercises 1 and 2 done

--- programming_lab/work/t_01_591149/exercise1.py
+++ programming_lab/work/t_01_591149/exercise1.py
@@ -2,6 +2,22 @@
 
 # Escreva um programa para aprovar o empréstimo bancario para a compra de uma casa. O
 # programa deve perguntar o valor da casa a comprar, o salário e a quantidade de anos a
-# pagar. O valorda prestação mensal não pode ser superior a 30% do salário. Calcule o
+# pagar. O valor da prestação mensal não pode ser superior a 30% do salário. Calcule o
 # valor da prestação como sendo o valor da casa a comprar dividido pelo número de
 # meses a pagar.
+
+home_value = float(input("Informe o valor da casa a comprar: "))
+if( home_value <= 0 ): print("Valor inválido"); exit() # Evita divisão por 0 ou número negativo
+
+years = int(input("Informe a quantidade de anos a pagar: "))
+if( years <= 0 ): print("Valor inválido"); exit() # Evita divisão por 0 ou número negativo
+
+salary = float(input("Informe o valor de seu salário: "))
+if( salary < 0 ): print("Valor inválido"); exit() # Salário não pode negativo.
+
+# Calculo da prestação mensal
+installment = home_value / years
+
+if( installment > (salary * 0.3) ): print("Valor da mensalidade maior que 30% de seu salário, Emprestimo bancário reprovado.")
+else: print("Emprestimo bancário aprovado.")
+exit();
--- programming_lab/work/t_01_591149/exercise2.py
+++ programming_lab/work/t_01_591149/exercise2.py
@@ -7,7 +7,7 @@
 
 # Preço por tipo e a faixa de consumo:
 
-# TIPO	      | FAIXA (kWh)   | PREÇO  
+# TIPO	      | FAIXA (kWh)   | PREÇO
 # Residêncial | Até 500	      | R$ 0,40
 #			  | Acima de 500  | R$ 0,65
 
@@ -16,3 +16,54 @@
 
 # Industrial  | Até 5000	  | R$ 0,55
 #  			  | Acima de 5000 | R$ 0,60
+
+
+def showPrices():
+	print("""
+	# TIPO\t\t| FAIXA (kWh)\t| PREÇO\t\t#
+	# Residêncial\t| Até 500\t| R$ 0,40\t#
+	#\t\t| Acima de 500\t| R$ 0,65\t#
+	# Comercial\t| Até 1000\t| R$ 0,55\t#
+	#\t\t| Acima de 1000\t| R$ 0,60\t#
+	# Industrial\t| Até 5000\t| R$ 0,55\t#
+	#\t\t| Acima de 5000\t| R$ 0,60\t#
+	""")
+
+def showMenu():
+	choise = input("""
+Qual seu tipo de instalação?
+[R] - Residêncial
+[C] - Comercial
+[I] - Industrial
+> """)
+	if  ( choise == "R" ): return 1;
+	elif( choise == "C" ): return 2;
+	elif( choise == "I" ): return 3;
+	else: return -1;
+
+
+def residential_choise():
+	kwh = int(input("\nInforme a quantidade de KWh consumida pela sua residência: "))
+	if  ( kwh <= 0 ): exit();
+	elif( kwh <= 500 ): print("O preço a pagar de acordo com a tabela é {:.2f}".format(kwh / 0.40));
+	else: print("O preço a pagar de acordo com a tabela é {:.2f}".format( (kwh / 0.65) ));
+
+def comercial_choise():
+	kwh = int(input("\nInforme a quantidade de KWh consumida pelo seu comércio: "))
+	if  ( kwh <= 0 ): exit();
+	elif( kwh <= 1000 ): print("O preço a pagar de acordo com a tabela é {:.2f}".format(kwh / 0.55));
+	else: print("O preço a pagar de acordo com a tabela é {:.2f}".format( (kwh / 0.60) ));
+
+def industrial_choise():
+	kwh = int(input("\nInforme a quantidade de KWh consumida pela sua industria: "))
+	if  ( kwh <= 0 ): exit();
+	elif( kwh <= 5000 ): print("O preço a pagar de acordo com a tabela é {:.2f}".format(kwh / 0.55));
+	else: print("O preço a pagar de acordo com a tabela é {:.2f}".format( (kwh / 0.60) ));
+
+showPrices();
+choise_id = showMenu();
+if  ( choise_id == 1 ): residential_choise()
+elif( choise_id == 2 ): comercial_choise()
+elif( choise_id == 3 ): industrial_choise()
+else: print("Tipo de instalação inválida.");
+exit();
