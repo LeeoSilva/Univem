b86a5fde93e10f39b429d4b8feddfc5c3fc4ff28
LeeoSilva <leeosilva0909@gmail.com>
2019-10-17 01:40:45 -0300

    new code for embedded-systems/car

--- embedded-systems/car/main.ino
+++ embedded-systems/car/main.ino
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <Servo.h>
 
 #define TRIG_PIN 9
 #define ECHO_PIN 10
@@ -6,12 +7,44 @@
 #define RIGHT_MOTOR_PIN 1
 #define LEFT_MOTOR_PIN 2
 
+#define SERVO_PIN 9
+
 AF_DCMotor motor1(RIGHT_MOTOR_PIN); 
 AF_DCMotor motor2(LEFT_MOTOR_PIN); 
 
-int getDistance(){
+Servo servo;
+
+
+unsigned turnServo(){ // return value is the angle of the servo
+	static unsigned pos = 0;
+	unsigned next_pos = 0;
+	bool dir = 0; // 0 -> RIGHT
+				  // 1 -> LEFT  
+
+	if(pos == 0){
+		next_pos = 90;	
+		dir = 0;
+	}
+	else if(pos == 90 && dir == 0) next_pos = 180;
+	else if(pos == 90 && dir == 1) next_pos = 0;
+	else if(pos == 180) {
+		next_pos = 90;
+		dir = 1;
+	}
+	else{
+		next_pos = 0;
+		dir = 0;
+	}
+
+	for(; pos != next_pos; delay(10)){
+		servo.write(pos);
+		pos = dir ? (--pos) : (++pos);
+	}
+	return pos;
+}
+
+int getDistance(){ 
 	long duration;
-	int distance;
 
 	digitalWrite(TRIG_PIN, LOW);
 	delayMicroseconds(2);
@@ -21,8 +54,7 @@ int getDistance(){
 	digitalWrite(TRIG_PIN, LOW);
 
 	duration = pulseIn(ECHO_PIN, HIGH);
-	distance = duration * 0.034 * 0.5;
-	return distance;
+	return duration * 0.034 * 0.5;
 }
 
 void goRight(){
@@ -46,11 +78,18 @@ void setup() {
 
 	motor1.setSpeed(255); // Max speed for the right motor.
 	motor2.setSpeed(255); // Max speed for the left  motor.
+
+	servo.attach(SERVO_PIN);
+}
+
+void driveLogic(){
+	static unsigned ultra_distance = getDistace();
+
+	if(ultra_distance <= 10
+
+
 }
 
 void loop() {
-
-
-
-
+	driveLogic();
 }
