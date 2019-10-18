fbd7efb4a895fb6d370b1ca843ba69dd008c51b8
LeeoSilva <leeosilva0909@gmail.com>
2019-10-18 19:20:18 -0300

    update - car

--- embedded-systems/car/main.ino
+++ embedded-systems/car/main.ino
@@ -15,64 +15,87 @@ AF_DCMotor motor2(LEFT_MOTOR_PIN);
 Servo servo;
 
 enum class servo_angle {
-	RIGHT = 0,
-	FRONTAL = 90,
-	LEFT = 180
+    LEFT = 0,
+    FRONTAL = 90,
+    RIGHT = 180
 };
 
+const DISTANCE_BASE_OBSTACLE =10;
+const DISTANCE_BASE_NOT_OBSTACLE =50;
 
 void turnServo(const servo_angle& target_angle){
-	for(unsigned angle = 0; angle != target_angle; delay(10))
-		servo.write(angle);
+    for(unsigned angle = 0; angle != target_angle; delay(10))
+        servo.write(angle);
 }
 
 int getDistance(){ 
-	long duration;
+    long duration =0;
 
-	digitalWrite(TRIG_PIN, LOW);
-	delayMicroseconds(2);
-	digitalWrite(TRIG_PIN, HIGH);
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
 
-	delayMicroseconds(10);
-	digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
 
-	duration = pulseIn(ECHO_PIN, HIGH);
-	return duration * 0.034 * 0.5;
+    duration = pulseIn(ECHO_PIN, HIGH);
+    return duration * 0.034 * 0.5;
 }
 
 void goRight(){
-	motor1.run(FORWARD); 
-	motor2.run(BACKWARD); 
+    motor1.run(FORWARD); 
+    motor2.run(BACKWARD); 
 }
 
 void goLeft(){
-	motor2.run(FORWARD);
-	motor1.run(BACKWARD);
+    motor2.run(FORWARD);
+    motor1.run(BACKWARD);
+}
+
+void goForward(){
+    motor1.run(FORWARD); 
+    motor2.run(FORWARD);
 }
 
 void stopAll(){
-	motor1.run(RELEASE); 
-	motor2.run(RELEASE);
+    motor1.run(RELEASE); 
+    motor2.run(RELEASE);
 }
 
 void setup() {
-	pinMode(TRIG_PIN, OUTPUT); 
-	pinMode(ECHO_PIN, INPUT); 
+    pinMode(TRIG_PIN, OUTPUT); 
+    pinMode(ECHO_PIN, INPUT); 
 
-	motor1.setSpeed(255); // Max speed for the right motor.
-	motor2.setSpeed(255); // Max speed for the left  motor.
+    motor1.setSpeed(255); // Max speed for the right motor.
+    motor2.setSpeed(255); // Max speed for the left  motor.
 
-	servo_angle servo_state = servo_angle.RIGHT;
-	servo.attach(SERVO_PIN);
+    servo_angle servo_state = servo_angle.FRONTAL;
+    servo.attach(SERVO_PIN);
 }
 
 void driveLogic(){
-	static unsigned ultra_distance = getDistace();
-	if(ultra_distance <= 10 
+    static unsigned ultra_distance = getDistace();
+    
+    if(ultra_distance <= DISTANCE_BASE_OBSTACLE) {
+        
+        turnServo(servo_angle.RIGHT);
+        if(ultra_distance >= DISTANCE_BASE_NOT_OBSTACLE){
+            turnServo(servo_angle.FRONTAL);
+            goRight();
+            goForward();  
+        }
+        
+        turnServo(servo_angle.LEFT);
+        if(ultra_distance >= DISTANCE_BASE_NOT_OBSTACLE){
+            turnServo(servo_angle.FRONTAL);
+            goLeft();
+            goForward();
+        }
+    }
 
 
 }
 
 void loop() {
-	driveLogic();
+    driveLogic();
 }
